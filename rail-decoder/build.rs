93e1d8216dc6f65853fa43c89739afd472200457
Andrew Kouré <andrew@koure.org>
2026-09-02 02:12:08 +0000

    fix: cargo fmt --all

--- rail-decoder/build.rs
+++ rail-decoder/build.rs
@@ -3,7 +3,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // and tfnsw_realtime.rs. They must not share a package name -- see
     // proto/tfnsw/README.md.
     prost_build::compile_protos(
-        &["proto/gtfs-realtime.proto", "proto/tfnsw/tfnsw-realtime.proto"],
+        &[
+            "proto/gtfs-realtime.proto",
+            "proto/tfnsw/tfnsw-realtime.proto",
+        ],
         &["proto/"],
     )?;
     Ok(())
